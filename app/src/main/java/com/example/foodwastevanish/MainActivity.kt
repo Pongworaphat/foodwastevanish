e772df31a5cdcc6a08a239b5286e37e1d0c06b3a
pongworaphat <pongworaphat.i@ku.th>
2025-04-13 00:33:49 +0700

    commit naja

--- app/src/main/java/com/example/foodwastevanish/MainActivity.kt
+++ app/src/main/java/com/example/foodwastevanish/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.example.foodwastevanish.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        val navView: BottomNavigationView = binding.navView // Access navView from the binding
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
@@ -29,7 +28,29 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_donate, R.id.navigation_inbox, R.id.navigation_notifications, R.id.navigation_more
             )
         )
+
+        data class User(val firstName: String, val lastName: String)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
+
+    private fun setContentView(root: Any) {
+
+    }
+
+    class ActivityMainBinding {
+        val navView: BottomNavigationView = TODO()
+        val root: Any
+            get() {
+                TODO()
+            }
+
+        companion object {
+            fun <LayoutInflater> inflate(layoutInflater: LayoutInflater): ActivityMainBinding {
+
+            }
+        }
+
+    }
+
 }
